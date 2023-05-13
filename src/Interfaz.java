31be6d4f3e0db4b192a445a5a0ca4d5c333d039d
TommysNJ <TommysNJ@users.noreply.github.com>
2023-05-12 23:47:58 -0500

    Initial commit

---
+++ src/Interfaz.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class Interfaz extends JFrame{
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTextArea areaCrear;
+    private JTextArea areaRemover;
+    private JTextArea areaMostrar;
+    private JTextArea areaInsertar;
+    private JTextArea areaDireccion;
+    private JTextArea areaNumero;
+    private JTextArea areaCiudad;
+    private JButton CREARButton;
+    private JTextField fieldCalleR;
+    private JTextField fieldCalleD;
+    private JTextField fieldFecha;
+    private JTextField fieldCiudadR;
+    private JTextField fieldEstadoR;
+    private JTextField fieldCodigoR;
+    private JTextField fieldCiudadD;
+    private JTextField fieldEstadoD;
+    private JTextField fieldCodigoD;
+    private JTextField fieldSeguimiento;
+
+    private TrackingSystem sistema = new TrackingSystem();
+    private Package p1;
+    private Package p2;
+    private Package p3;
+    private Package p4;
+    private Package p5;
+    private Package p6;
+
+
+    public Interfaz(){
+        setContentPane(panel1);
+
+        CREARButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (sistema.searchByTrackingNumber(fieldSeguimiento.getText())==null){
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    String textoFecha = fieldFecha.getText();
+                    LocalDate fecha = LocalDate.parse(textoFecha,formato);
+                    sistema.addPackage(new Package(fieldSeguimiento.getText(),new Address(fieldCalleR.getText(),fieldCiudadR.getText(),fieldEstadoR.getText(),fieldCodigoR.getText()),
+                            new Address(fieldCalleD.getText(),fieldCiudadD.getText(),fieldEstadoD.getText(),fieldCodigoD.getText()),fecha));
+                    areaCrear.setText("El paquete se ha creado con éxito.");
+                } else {
+                    areaCrear.setText("El número de seguimiento ingresado ya existe, ingrese otro!");
+                    fieldSeguimiento.setText("");
+                }
+            }
+        });
+
+    }
+}
---
+++ src/Package.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+
+public class Package {
+    private String trackingNumber;
+    private Address senderAddress;
+    private Address recipientAddress;
+    private LocalDate estimatedDeliveryDate;
+
+    public Package(String trackingNumber, Address senderAddress, Address recipientAddress, LocalDate estimatedDeliveryDate) {
+        this.trackingNumber = trackingNumber;
+        this.senderAddress = senderAddress;
+        this.recipientAddress = recipientAddress;
+        this.estimatedDeliveryDate = estimatedDeliveryDate;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public Address getSenderAddress() {
+        return senderAddress;
+    }
+
+    public void setSenderAddress(Address senderAddress) {
+        this.senderAddress = senderAddress;
+    }
+
+    public Address getRecipientAddress() {
+        return recipientAddress;
+    }
+
+    public void setRecipientAddress(Address recipientAddress) {
+        this.recipientAddress = recipientAddress;
+    }
+
+    public LocalDate getEstimatedDeliveryDate() {
+        return estimatedDeliveryDate;
+    }
+
+    public void setEstimatedDeliveryDate(LocalDate estimatedDeliveryDate) {
+        this.estimatedDeliveryDate = estimatedDeliveryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Package{" +
+                "trackingNumber='" + trackingNumber + '\'' +
+                ", senderAddress=" + senderAddress +
+                ", recipientAddress=" + recipientAddress +
+                ", estimatedDeliveryDate=" + estimatedDeliveryDate +
+                '}';
+    }
+}
